ea556093342035593998004cc2a059995dbf10ba
Guilherme Carreiro <karreiro@gmail.com>
2020-06-30 00:28:30 -0300

    .

--- src/index.ts
+++ src/index.ts
@@ -13,137 +13,26 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import puppeteer from "puppeteer";
-import open from "open";
-import fs from "fs";
-// import ProgressBar from "progress";
 
-declare global {
-  const gwtEditorBeans: {
-    get: (
-      editorName: string
-    ) => {
-      get: () => {
-        setContent: (_a: string, _b: string) => void;
-      };
-    };
-  };
-}
+import { Command } from "commander";
+import { getPreview } from "./preview";
 
-(async () => {
-  // var bar = new ProgressBar("Generating DMN model preview [:bar] :percent :etas", {
-  //   complete: "=",
-  //   incomplete: " ",
-  //   width: 20,
-  //   total: 9,
-  // });
+const program = new Command();
 
-  const a0 = new Date().getTime();
-  const browser = await puppeteer.launch({
-    args: [
-      "--no-sandbox",
-      "--disable-setuid-sandbox",
-      "--disable-dev-shm-usage",
-      // "--disable-accelerated-2d-canvas",
-      "--no-first-run",
-      "--no-zygote",
-      "--disable-gpu",
-    ],
+program.name("dmn-cli");
+program.description("DMN command line tool");
+program.version("dmn-cli: 0.1.0", "-v, --version");
+
+program
+  .command("preview <path>")
+  .description("shows an image of the DMN model")
+  .action((path) => getPreview(path));
+
+program
+  .command("inspect <path>")
+  .description("inspect elements into the DMN model")
+  .action((path) => {
+    console.log("inspect", path);
   });
-  const a1 = new Date().getTime();
-  console.log("launch: " + (a1 - a0) + "ms");
-  // bar.tick(1);
-  const page = await browser.newPage();
-  const a2 = new Date().getTime();
-  console.log("new page: " + (a2 - a1) + "ms");
-  // bar.tick(1);
-  await page.goto(`file:///${__dirname}/dmn-editor/index.html`);
-  const a3 = new Date().getTime();
-  console.log("go to: " + (a3 - a2) + "ms");
-  // bar.tick(1);
-  await page.waitForSelector("body");
-  const a4 = new Date().getTime();
-  console.log("load body: " + (a4 - a3) + "ms");
-  // bar.tick(1);
 
-  await page.evaluate(function () {
-    gwtEditorBeans
-      .get("DMNDiagramEditor")
-      .get()
-      .setContent(
-        "",
-        '<dmn:definitions xmlns:dmn="http://www.omg.org/spec/DMN/20180521/MODEL/" xmlns="https://kiegroup.org/dmn/_2D33DF49-5887-43C7-A5F1-CC77660CB14F" xmlns:feel="http://www.omg.org/spec/DMN/20180521/FEEL/" xmlns:kie="http://www.drools.org/kie/dmn/1.2" xmlns:dmndi="http://www.omg.org/spec/DMN/20180521/DMNDI/" xmlns:di="http://www.omg.org/spec/DMN/20180521/DI/" xmlns:dc="http://www.omg.org/spec/DMN/20180521/DC/" id="_216175AC-8BBF-428A-BA98-8FB0171425CE" name="new-file" typeLanguage="http://www.omg.org/spec/DMN/20180521/FEEL/" namespace="https://kiegroup.org/dmn/_2D33DF49-5887-43C7-A5F1-CC77660CB14F"><dmn:extensionElements/><dmn:decision id="_DDA343D1-5547-441F-83BA-8D3E19CEC3F8" name="Decision-1"><dmn:extensionElements/><dmn:variable id="_1737734A-00CD-4A69-9D0A-1C13B6634D12" name="Decision-1"/></dmn:decision><dmn:decision id="_FF8E9F45-4451-4962-80ED-2B7A27DAE0DA" name="Decision-2"><dmn:extensionElements/><dmn:variable id="_64348A58-E8B5-4AC7-9303-6BD4889E8AC4" name="Decision-2"/></dmn:decision><dmndi:DMNDI><dmndi:DMNDiagram><di:extension><kie:ComponentsWidthsExtension/></di:extension><dmndi:DMNShape id="dmnshape-_DDA343D1-5547-441F-83BA-8D3E19CEC3F8" dmnElementRef="_DDA343D1-5547-441F-83BA-8D3E19CEC3F8" isCollapsed="false"><dmndi:DMNStyle><dmndi:FillColor red="255" green="255" blue="255"/><dmndi:StrokeColor red="0" green="0" blue="0"/><dmndi:FontColor red="0" green="0" blue="0"/></dmndi:DMNStyle><dc:Bounds x="90" y="196" width="100" height="50"/><dmndi:DMNLabel/></dmndi:DMNShape><dmndi:DMNShape id="dmnshape-_FF8E9F45-4451-4962-80ED-2B7A27DAE0DA" dmnElementRef="_FF8E9F45-4451-4962-80ED-2B7A27DAE0DA" isCollapsed="false"><dmndi:DMNStyle><dmndi:FillColor red="255" green="255" blue="255"/><dmndi:StrokeColor red="0" green="0" blue="0"/><dmndi:FontColor red="0" green="0" blue="0"/></dmndi:DMNStyle><dc:Bounds x="2605" y="66" width="100" height="50"/><dmndi:DMNLabel/></dmndi:DMNShape></dmndi:DMNDiagram></dmndi:DMNDI></dmn:definitions>'
-      );
-  });
-  const a5 = new Date().getTime();
-  console.log("execute set content: " + (a5 - a4) + "ms");
-  // bar.tick(1);
-
-  await page.waitForSelector("canvas");
-  const a6 = new Date().getTime();
-  console.log("wait canvas load: " + (a6 - a5) + "ms");
-  // bar.tick(1);
-  // await page.click("canvas");
-  const a7 = new Date().getTime();
-  console.log("click on canvas: " + (a7 - a6) + "ms");
-  // bar.tick(1);
-  await page.focus("ul.nav.nav-tabs > li + li a");
-  await page.click("ul.nav.nav-tabs > li + li a");
-  const a8 = new Date().getTime();
-  console.log("open doc tab: " + (a8 - a7) + "ms");
-  // bar.tick(1);
-  await page.waitForSelector("ul.nav.nav-tabs > li + li.active");
-  const a9 = new Date().getTime();
-  console.log("wait doc active: " + (a9 - a8) + "ms");
-  // bar.tick(1);
-
-  const img = await page.$eval(".diagram-image", (el) => (el as any).src);
-  const a10 = new Date().getTime();
-  console.log("get image: " + (a10 - a9) + "ms");
-  // bar.tick(1);
-
-  const data = img.replace(/^data:image\/\w+;base64,/, "");
-
-  fs.writeFileSync("image.png", Buffer.from(data, "base64"));
-
-  open("image.png");
-
-  // await canvas!.screenshot({
-  //   path: "logo-screenshot.png",
-  // });
-
-  await browser.close();
-})();
-
-// -- WOKRS OPEN ----------------------------------------------------
-// import open from "open";
-
-// (async () => {
-//   await open("/Users/karreiro/Projects/dmn-cli/docs/diagram-dmn.png");
-// })();
-// -- WOKRS OPEN ---------------
-
-// -- WOKRS CLI ----------------------------------------------------
-// import { Command } from "commander";
-// const program = new Command();
-
-// program.name("dmn-cli");
-// program.description("DMN command line tool");
-// program.version("dmn-cli: 0.1.0", "-v, --version");
-
-// program
-//   .command("preview <path>")
-//   .description("shows an image of the DMN model")
-//   .action((path) => {
-//     getModelPreview(path);
-//   });
-
-// program
-//   .command("inspect <path>")
-//   .description("inspect elements into the DMN model")
-//   .action((path) => {
-// console.log("inspect", path);
-//   });
-
-// program.parse(process.argv);
-// -- WOKRS CLI ---------------
+program.parse(process.argv);
--- src/preview.ts
+++ src/preview.ts
@@ -1,21 +1,155 @@
-import puppeteer from "puppeteer";
+/*
+ * Copyright 2020 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-export const getModelPreview = (path: string) => {
-  console.log("getting dmn model image", path);
+import { Browser, launch } from "puppeteer";
+import open from "open";
+import fs from "fs";
+import ProgressBar from "progress";
 
-  return (async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+const DIAGRAM_NAME = "diagram.png";
 
-    await page.goto("file:///Users/karreiro/Projects/forks/dmn-cli/docs/index.html");
-    await page.waitForSelector("#canvas");
-    const canvas = await page.$("#canvas");
+export const getPreview = (path: string) =>
+  (async () => {
+    let dmnXml: string = "";
+    let diagramData: string = "";
+    let browser: Browser;
+    let progressBar: ProgressBar;
 
-    await canvas!.screenshot({
-      path: "logo-screenshot.png",
-      omitBackground: true,
-    });
+    try {
+      dmnXml = fs.readFileSync(path, "utf8");
+    } catch (e) {
+      raiseError(`This path could not be read: ${path}`);
+    }
 
-    await browser.close();
+    try {
+      browser = await initBrowser();
+      progressBar = initProgressBar();
+    } catch (e) {
+      raiseError("DMN command line tool was not correctly installed.");
+    }
+
+    try {
+      progressBar.tick(1);
+      diagramData = await getDMNDiagramImage(dmnXml, browser, progressBar);
+    } catch (e) {
+      raiseError("DMN model preview could not be generated.");
+    }
+
+    try {
+      progressBar.tick(1);
+      createImageFile(diagramData);
+    } catch (e) {
+      raiseError("DMN model preview image file could not be created.");
+    }
+
+    try {
+      progressBar.tick(1);
+      open(DIAGRAM_NAME);
+    } catch (e) {
+      raiseError("DMN model preview image file could not be opened.");
+    }
+
+    try {
+      await browser.close();
+    } catch (e) {
+      process.exit();
+    }
   })();
-};
+
+async function initBrowser() {
+  return await launch({
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--no-first-run",
+      "--no-zygote",
+      "--disable-gpu",
+    ],
+  });
+}
+
+async function getDMNDiagramImage(
+  dmnXml: string,
+  browser: Browser,
+  progressBar: ProgressBar
+) {
+  const page = await browser.newPage();
+
+  progressBar.tick(1);
+  await page.goto(`file:///${__dirname}/dmn-editor/index.html`);
+
+  progressBar.tick(1);
+  await page.waitForSelector("body");
+
+  progressBar.tick(1);
+  await page.evaluate((xml) => {
+    gwtEditorBeans.get("DMNDiagramEditor").get().setContent("", xml);
+  }, dmnXml);
+
+  progressBar.tick(1);
+  await page.waitForSelector("canvas");
+
+  progressBar.tick(1);
+  await page.focus("ul.nav.nav-tabs > li + li a");
+  await page.click("ul.nav.nav-tabs > li + li a");
+
+  progressBar.tick(1);
+  await page.waitForSelector("ul.nav.nav-tabs > li + li.active");
+
+  progressBar.tick(1);
+  const diagramData = page.$eval(
+    ".diagram-image",
+    (el) => (el as any).src as string
+  );
+
+  return diagramData;
+}
+
+function initProgressBar() {
+  return new ProgressBar(
+    "Generating DMN model preview [:bar] (:current/:total)",
+    {
+      complete: "=",
+      incomplete: " ",
+      width: 20,
+      total: 10,
+    }
+  );
+}
+
+function createImageFile(imageData: string) {
+  const data = imageData.replace(/^data:image\/\w+;base64,/, "");
+
+  fs.writeFileSync(DIAGRAM_NAME, Buffer.from(data, "base64"));
+}
+
+function raiseError(errorMessage: string): never {
+  console.error(`\n\n :: Error (dmn-cli) :: ${errorMessage}\n`);
+  process.exit();
+}
+
+declare global {
+  const gwtEditorBeans: {
+    get: (
+      editorName: string
+    ) => {
+      get: () => {
+        setContent: (_a: string, _b: string) => void;
+      };
+    };
+  };
+}
